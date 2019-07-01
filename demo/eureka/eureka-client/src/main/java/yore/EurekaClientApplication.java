09f43a2837842ad4761d8f7ea636ae20e93a9a03
yoreyuan <yuandongdong@gbicc.net>
2019-07-02 06:25:48 +0800

    提交Eureka代码

---
+++ demo/eureka/eureka-client/src/main/java/yore/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package yore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by yore on 2019-07-02 5:34
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
---
+++ demo/eureka/eureka-server/src/main/java/yore/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package yore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by yore on 2019-07-02 5:11
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
