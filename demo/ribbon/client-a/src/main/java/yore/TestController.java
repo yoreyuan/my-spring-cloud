dc467e569910fafe3999d67e6cb50ea0bb9ca9a3
yoreyuan <791072311@qq.com>
2019-07-13 23:22:58 +0800

    controller层添加接口

--- demo/ribbon/client-a/src/main/java/yore/TestController.java
+++ demo/ribbon/client-a/src/main/java/yore/TestController.java
@@ -15,4 +15,25 @@ public class TestController {
         return " From Port: "+ request.getServerPort() + ", Result: " + (a + b);
     }
 
+    @GetMapping("/a/add")
+    public Integer aadd(Integer a, Integer b){
+        return a + b;
+    }
+
+    @GetMapping("/sub")
+    public Integer sub(Integer a, Integer b){
+        return a - b;
+    }
+
+    @GetMapping("/mul")
+    public String mul(Integer a, Integer b){
+        System.out.println("进入client-a!");
+        return "client-a-" + a * b;
+    }
+
+    @GetMapping("/div")
+    public Integer div(Integer a, Integer b){
+        return a / b;
+    }
+
 }
