6c2539fb21e0b17ab337e25a8712a06aa9003d69
yoreyuan <791072311@qq.com>
2019-07-14 15:25:15 +0800

    引入Swagger2

--- demo/ribbon/client-a/src/main/java/yore/ClientAApp.java
+++ demo/ribbon/client-a/src/main/java/yore/ClientAApp.java
@@ -3,16 +3,50 @@ package yore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
+ *
+ * java -jar client-a-1.0-SNAPSHOT.jar --spring.profiles.active=node1
+ *
  * Created by yore on 2019/7/7 18:59
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 public class ClientAApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ClientAApp.class, args);
+//        SpringApplication.run(ClientAApp.class, args);
+
+        // --spring.profiles.active=node1，每次启动将node1改为node2或node3，就能启动多个服务实例
+        SpringApplication.run(ClientAApp.class, "--spring.profiles.active=node1");
+    }
+
+
+    public static final String VERSION = "1.0.0";
+    public static final String SWAGGER_SCAN_BASE_PACKAGE = "yore.controller";
+
+    @Bean
+    public Docket swaggerPersonApi10() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(SWAGGER_SCAN_BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(
+                        new ApiInfoBuilder()
+                                .version(VERSION)
+                                .title("Original Service API")
+                                .description("Original Service API v" + VERSION)
+                                .build()
+                );
     }
 
 }
--- demo/ribbon/client-a/src/main/java/yore/controller/TestController.java
+++ demo/ribbon/client-a/src/main/java/yore/controller/TestController.java
@@ -1,36 +1,45 @@
-package yore;
+package yore.controller;
 
 import javax.servlet.http.HttpServletRequest;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by yore on 2019/7/7 19:00
  */
+@Api(description = "测试源服务API接口")
 @RestController
 public class TestController {
 
+    @ApiOperation(value = "加法", notes = "带标记加法")
     @GetMapping("/add")
     public String add(Integer a, Integer b, HttpServletRequest request){
         return " From Port: "+ request.getServerPort() + ", Result: " + (a + b);
     }
 
+    @ApiOperation(value = "加法", notes = "加法")
     @GetMapping("/a/add")
     public Integer aadd(Integer a, Integer b){
         return a + b;
     }
 
+    @ApiOperation(value = "减法", notes = "减法")
     @GetMapping("/sub")
     public Integer sub(Integer a, Integer b){
         return a - b;
     }
 
+    @ApiOperation(value = "乘法", notes = "乘法")
     @GetMapping("/mul")
     public String mul(Integer a, Integer b){
         System.out.println("进入client-a!");
         return "client-a-" + a * b;
     }
 
+    @ApiOperation(value = "除法", notes = "除法")
     @GetMapping("/div")
     public Integer div(Integer a, Integer b){
         return a / b;
